449c45a3da710d95f34bc6861524f6a127bd778c
mikiher <mikiher@gmail.com>
2024-10-01 14:18:53 +0300

    Return other mac toolchain settings

--- cmake/Toolchain-Mac64.cmake
+++ cmake/Toolchain-Mac64.cmake
@@ -1,1 +1,4 @@
 set(CMAKE_OSX_ARCHITECTURES "x86_64;arm64" CACHE STRING "Build architectures for OSX" FORCE)
+set(CMAKE_OSX_DEPLOYMENT_TARGET "14.0" CACHE STRING "Minimum OS X deployment version" FORCE)
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_C_COMPILER /usr/bin/clang)
