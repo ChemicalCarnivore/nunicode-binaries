4388ca98b992d6b65485a053d0332954bed023b0
mikiher <mikiher@gmail.com>
2024-10-01 10:48:31 +0300

    Put OSX_ARCHITECTURES definition first

--- cmake/Toolchain-Mac64.cmake
+++ cmake/Toolchain-Mac64.cmake
@@ -1,3 +1,6 @@
+# Enable necessary architecture support (default to x86_64 and arm64 for Apple Silicon)
+set(CMAKE_OSX_ARCHITECTURES "x86_64;arm64")
+
 # Set the system name to macOS (Darwin)
 set(CMAKE_SYSTEM_NAME Darwin)
 
@@ -7,5 +10,3 @@ set(CMAKE_OSX_DEPLOYMENT_TARGET "14.0")
 # Set the C compiler to use Clang, which is the default on macOS
 set(CMAKE_C_COMPILER /usr/bin/clang)
 
-# Enable necessary architecture support (default to x86_64 and arm64 for Apple Silicon)
-set(CMAKE_OSX_ARCHITECTURES "x86_64;arm64")
