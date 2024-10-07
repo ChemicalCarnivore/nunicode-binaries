fd0f9b7cb5363b26761959a00bd8886f393b560e
mikiher <mikiher@gmail.com>
2024-10-07 15:20:29 +0300

    Fix musl compiler path

--- cmake/Toolchain-Musl-Arm64.cmake
+++ cmake/Toolchain-Musl-Arm64.cmake
@@ -3,7 +3,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 # Set the cross-compiler
-set(CMAKE_C_COMPILER /usr/aarch64-linux-musl/bin/aarch64-linux-musl-gcc)
+set(CMAKE_C_COMPILER /usr/local/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc)
 
 # Set the sysroot
 set(CMAKE_SYSROOT /usr/aarch64-linux-musl)
